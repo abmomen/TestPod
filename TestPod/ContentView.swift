da147962011730b684b7ed34285a7bc3bffce71e
Abdul Momen <abdulmomen130@gmail.com>
2023-12-13 12:38:24 +0600

    Initial Commit

---
+++ TestPod/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestPod
+//
+//  Created by ST-iOS on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestPod/TestPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPodApp.swift
+//  TestPod
+//
+//  Created by ST-iOS on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TestPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
